a9c801e0bad4e36312ba8b523a51ac0e652ab03c
puakehaulani <puakehaulani@gmail.com>
2021-02-19 14:52:54 -0800

    made new pages and linked them, got basic structure on

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,8 +3,11 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+      <a className="navbar-brand" href="/search">
+        Search
+      </a>
+      <a className="navbar-brand" href="/saved">
+        Saved
       </a>
     </nav>
   );
