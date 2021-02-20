94d01e22efa408a5ccb6935b1538161f0ccc77d9
puakehaulani <puakehaulani@gmail.com>
2021-02-19 20:59:39 -0800

    rendering books from api call

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,28 +2,14 @@ import React, { useState, useEffect } from "react";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
+import { Col, Row, Container } from "react-bootstrap";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Search() {
     // Setting our component's initial state
-    const [books, setBooks] = useState([])
     const [formObject, setFormObject] = useState({})
-
-    // Load all books and store them with setBooks
-    useEffect(() => {
-        loadBooks()
-    }, [])
-
-    // Loads all books and sets them to books
-    function loadBooks() {
-        API.getBooks()
-            .then(res =>
-                setBooks(res.data)
-            )
-            .catch(err => console.log(err));
-    };
+    const [resultsList, setResultsList] = useState({});
 
     // Handles updating component state when the user types into the input field
     function handleInputChange(event) {
@@ -31,16 +17,14 @@ function Search() {
         setFormObject({ ...formObject, [name]: value })
     };
 
-    // When the form is submitted, use the API.saveBook method to save the book data
-    // Then reload books from the database
+
     function handleFormSubmit(event) {
         event.preventDefault();
         const title = formObject.title
-
         API.googleBook(title)
             .then(res =>
-                // loadBooks())
-                console.log(res))
+                setResultsList(res.data.items)
+            )
             .catch(err => console.log(err));
 
     };
@@ -72,33 +56,35 @@ function Search() {
                     <Jumbotron>
                         <h1>Results</h1>
                     </Jumbotron>
-                    {books.length ? (
+                    {resultsList.length ? (
                         <List>
-                            {books.map(book => (
-                                <ListItem key={book._id}>
-                                    <a href={book.url}>
-                                        <strong>
-                                            {book.title} by {book.author}
-                                        </strong>
-                                    </a>
-                                    <Row>
-                                        <Col>
-                                            <img src={book.image} />
-                                        </Col>
-                                        <Col size="md-10 md-offset-1">
-                                            <article>
-                                                <h1>Description</h1>
-                                                <p>
-                                                    {book.description}
-                                                </p>
-                                            </article>
-                                        </Col>
-                                    </Row>
+                            {resultsList.map(book => {
+                                return (
+                                    <ListItem key={book.id}>
+                                        <a href={book.volumeInfo.previewLink}>
+                                            <strong>
+                                                {book.volumeInfo.title} by {book.volumeInfo.authors}
+                                            </strong>
+                                        </a>
+                                        <Row>
+                                            <Col>
+                                                <img src={book.volumeInfo.imageLinks.thumbnail} />
+                                            </Col>
+                                            <Col size="md-10 md-offset-1">
+                                                <article>
+                                                    <h1>Description</h1>
+                                                    <p>
+                                                        {book.volumeInfo.description}
+                                                    </p>
+                                                </article>
+                                            </Col>
+                                        </Row>
 
-                                    <button>View</button>
-                                    <button>Add</button>
-                                </ListItem>
-                            ))}
+                                        <button>View</button>
+                                        <button>Add</button>
+                                    </ListItem>
+                                )
+                            })}
                         </List>
                     ) : (
                             <h3>No Results to Display</h3>
